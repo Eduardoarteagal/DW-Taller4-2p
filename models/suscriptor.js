0fa64c9fc4fa429989c2270b95d8a9180459157e
eduardoarteaga@uees.edu.ec <eduardoarteaga@uees.edu.ec>
2025-03-25 11:15:17 -0500

    Second commit

--- models/suscriptor.js
+++ models/suscriptor.js
@@ -1,24 +1,22 @@
 'use strict';
-const {
-  Model
-} = require('sequelize');
+const { Model } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class Suscriptor extends Model {
     static associate(models) {
       Suscriptor.belongsToMany(models.Interes, {
         through: 'suscriptorinteres',
         as: 'intereses',
-        foreignKey: 'suscriptor_id'
+        foreignKey: 'suscriptor_id',
       });
     }
-  };
+  }
   Suscriptor.init({
     nombre: DataTypes.STRING,
     email: DataTypes.STRING,
-    fecha_registro: DataTypes.DATE
+    fecha_registro: DataTypes.DATE,
   }, {
     sequelize,
-    modelName: 'suscriptor',
+    modelName: 'Suscriptor',
   });
   return Suscriptor;
 };
